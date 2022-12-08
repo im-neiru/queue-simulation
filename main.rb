142841d33d4b348e1e658cd8a862e9bc72fc1361
randel-reyes22 <randelreyes818@gmail.com>
2022-12-08 23:51:07 +0800

    there is something wrong

--- main.rb
+++ main.rb
@@ -13,30 +13,56 @@ require_relative 'models/customer.rb'
 
 tellers = [Teller.new("Teller A", 1), Teller.new("Teller B", 2), Teller.new("Teller C", 3)]
 
-total_processed_customers = 0
+
 customer_number = 0
 current_cus_time_to_finished = 0
-max_customers = 50
 minute = 1;
-total_people_in_line = 50
+total_people_in_line = 0
+MAX_LINE_SIZE = 5
+MAX_CUSTOMERS = 50
 
 def task_type
     rand <= 0.15 ? 2 : 1
 end
 
-while (total_processed_customers <= max_customers)
+while (total_people_in_line <= MAX_CUSTOMERS)
 
     puts "\n********************"
     puts "     Minute #{minute}"
     puts "********************"
 
+    min_line_index = 0
+    min_line_size = MAX_LINE_SIZE
+
     num_customers = rand(0..5)
-    num_customers.times do |i|
-        tellers.each do |teller|
-            if !teller.is_busy?
-                customer_number += i
-                teller.add_queue(Customer.new(customer_number, task_type))
+    #puts "random customer #{num_customers}"
+    num_customers.times do
+        #create a new customer
+        #find the shortest line
+        customer_number += 1
+
+        (0...tellers.size).each do |i|
+            if tellers[i].line_length < min_line_size
+                min_line_size = tellers[i].line_length
+                min_line_index = i
             end
+        end
+
+        #check if tellers are having the equals lines
+        all_lines_equal = tellers.all? {|line_size| line_size == min_line_size}
+
+        #check if lines are all equal or if the tellers line size are = 0
+        #and after that get the teller index or position in the array
+        teller_index = all_lines_equal || min_line_size == 0 ? rand(0..2) : min_line_index
+
+        #get the teller
+        current_teller = tellers[teller_index]
+
+        if !current_teller.is_busy?
+            customer = Customer.new(customer_number, task_type)
+            #puts "Customer #{customer.customer_num} and task type #{customer.random_task_value} is added to line of #{current_teller.teller_name}"
+            current_teller.add_to_queue(customer)
+            
         end
     end
 
@@ -45,20 +71,40 @@ while (total_processed_customers <= max_customers)
             current_cus_time_to_finished = teller.serve
         end
 
+        #the teller is busy and currently serving a customer
         if teller.is_busy?
             current_cus_time_to_finished -= 1
             if current_cus_time_to_finished == 0
                 teller.busy = false
-                total_processed_customers += 1
+                total_people_in_line += 1
             end
         end
+
+        puts "#{teller.teller_name} current load is #{teller.current_load}"
     end
 
-    #puts "total oki customers: #{total_processed_customers}"
-
+    #puts "Customers served is #{total_processed_customers}"
     minute += 1
     puts "\n"
 end
+
+#  # Add the customer to the queue 
+#  all_lines_equal = tellers.all? {|teller| line_size == min_line_size}
+
+#  #check if lines are all equal or if the tellers line size are = 0
+#  teller_index = all_lines_equal || min_line_size == 0 ? rand(0..2) : min_line_index
+
+# tellers.each do |teller|
+#     if !teller.is_busy? 
+#         teller.add_to_queue(customer)
+#     end
+# end
+
+# tellers.each do |teller|
+#     if !teller.is_busy? #the teller is not busy
+#         teller.add_to_queue(Customer.new(customer_number, task_type))
+#     end
+# end
 
 # puts "#{teller.teller_name}"
 # puts "Current customer proccessing time #{current_cus_time_to_finished}"
--- models/teller.rb
+++ models/teller.rb
@@ -1,17 +1,13 @@
-class Teller 
+class Teller
 
     attr_reader :teller_name
     attr_reader :teller_speed
-    attr_reader :time_it_takes
     attr_writer :busy
 
     def initialize(teller_name, teller_speed)
-        @customers_line = Array.new #line for the customers
-
-        @teller_speed = teller_speed
+        @customers_line = Queue.new #line for the customers
         @teller_name = teller_name
-
-        @time_it_takes = 0
+        @teller_speed = teller_speed
         #initilize the tellers as not busy
         @busy = false
     end
@@ -19,32 +15,30 @@ class Teller
     def line_length
         @customers_line.length
     end
-  
+    
     def is_busy?
         @busy
     end
 
     #meaning this is the current execution time of a teller to 
     #serve a customer
-    def current_running_load #this is the load of the backline
+    def current_load #this is the load of the backline
         #puts "len  #{@customers_line.length}"
         @customers_line.length * @teller_speed
-        #puts "time it takes #{@time_it_takes}"
     end
 
     #add the customer to the line
-    def add_queue(customer)
-        @customers_line << customer
+    def add_to_queue(customer)
+        @customers_line.push(customer)
     end
-    
+
     #serve the customer and exit the customer after the
     #task on hand is done
     def serve
         @busy = true
-        customer =  @customers_line.shift #isserve na
+        customer = @customers_line.pop #isserve na
         #puts "customer #{customer.customer_num}"
         #current time teller current na tgsserve kang teller
-        
         customer.random_task_value * @teller_speed 
     end
 end
